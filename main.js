b838a02c16c36c7cc744c318bf9f2d1de5611985
silviosnjr <silviosnjr@gmail.com>
2024-01-05 16:01:12 -0300

    Inserindo lógica de programação no main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-let nomeUsuário = prompt("Qual o seu nome?")
+let nomeUsuário = "";
 let elemento = document.querySelector("#nome-usuário");
-elemento.textContent = nomeUsuário;
+
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Qual o seu nome?");
+}
+
+if(nomeUsuário == null){
+    elemento.textContent = 'seja muito bem-vindo.';
+}else{
+    elemento.textContent = nomeUsuário;
+}
